6a335e98f3f2141afa7f61018eba0db464eee503
Sept Team <septteam@outlook.com>
2020-05-05 13:58:06 +0300

    #Commit: Code Change

--- fox.js
+++
@@ -1,5 +0,0 @@
-const ScheduleFox = require('./lib/scheduleFox');
-const mongoUri = 'mongodb+srv://admin:admin@cluster0-hyavj.gcp.mongodb.net/ScheduleFox';
-
-const schedulefox = new ScheduleFox({config: {uri: mongoUri}});
-
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const ScheduleFox = require('./lib/schedulefox');
+const ScheduleFox = require('./lib');
+const mongoUri = 'mongodb+srv://admin:admin@cluster0-hyavj.gcp.mongodb.net/ScheduleFox';
 
-module.export = ScheduleFox;
+const schedulefox = new ScheduleFox({config: {uri: mongoUri}});
+
+ data = {
+        JobName: "JobName-Test-01",
+        TimePattern: "*/10 * * * * *"
+    };
+
+schedulefox.create(data);
+
+// schedulefox.dbService.prototype
+
+// schedulefox.context.prototype.mongoService.
+
+// schedulefox.
+
